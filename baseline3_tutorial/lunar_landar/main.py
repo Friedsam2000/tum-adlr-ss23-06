c16b7eb382116a869253521ba29b191eaa2a43b4
samuel <samuelzeitler2000@gmail.com>
2023-05-08 10:09:58 +0200

    removed training data, better code, not parallized

--- baseline3_tutorial/lunar_landar/main.py
+++ baseline3_tutorial/lunar_landar/main.py
@@ -2,8 +2,9 @@ import gym
 from stable_baselines3 import PPO
 import os
 
+env = gym.make('LunarLander-v2')
 
-models_dir = "models/PPO"
+models_dir = "models/PPO_0"
 logdir = "logs"
 
 if not os.path.exists(models_dir):
@@ -12,37 +13,40 @@ if not os.path.exists(models_dir):
 if not os.path.exists(logdir):
     os.makedirs(logdir)
 
-env = gym.make('LunarLander-v2')
-env.reset()
+def getLatestModel():
+    if os.path.exists(models_dir) and len(os.listdir(models_dir)) > 0:
+        models = os.listdir(models_dir)
+        #integer sort
+        models.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+        return models[-1]
+    else:
+        return None
 
-# model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
-
-#Training
-# TIMESTEPS = 10000
-# iters = 0
-# for i in range(30):
-#     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
-#     model.save(f"{models_dir}/{TIMESTEPS*i}")
-
-
-#Continuing training
-# timesteps_to_continue = 320000
-# model = PPO.load(f"{models_dir}/{timesteps_to_continue}", env=env)
-# TIMESTEPS = 10000
-# for i in range(10):
-#     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
-#     model.save(f"{models_dir}/{TIMESTEPS*i+timesteps_to_continue}")
+# Start training or continue training at the last model
+TIMESTEPS = 10000
+MAX_TIMESTEPS = 500000
+latest_model = getLatestModel()
+if latest_model is not None:
+    model = PPO.load(f"{models_dir}/{latest_model}", env=env, verbose=1, tensorboard_log=logdir)
+    print(f"Continue training at timestep {model.num_timesteps}")
+else:
+    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
+    print("Start training from scratch")
+while model.num_timesteps < MAX_TIMESTEPS:
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
+    model.save(f"{models_dir}/{model.num_timesteps}")
 
 
-# Testing
-test_timesteps = 350000
-model = PPO.load(f"{models_dir}/{test_timesteps}")
-obs = env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
-    if dones:
-        break
-env.close()
-
+# Test the latest model
+# latest_model = getLatestModel()
+# if latest_model is not None:
+#     model = PPO.load(f"{models_dir}/{latest_model}", env=env, verbose=1, tensorboard_log=logdir)
+#     print(f"Testing model at timestep {model.num_timesteps}")
+#     obs = env.reset()
+#     for i in range(1000):
+#         action, _states = model.predict(obs, deterministic=True)
+#         obs, rewards, dones, info = env.step(action)
+#         env.render()
+#         if dones:
+#             break
+#     env.close()
