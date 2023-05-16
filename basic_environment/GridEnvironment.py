44d0b3b0d96d77105890017d0d57eeb195799a9d
samuel <samuelzeitler2000@gmail.com>
2023-05-16 14:20:38 +0200

    grid environment with rgb colors

--- basic_environment/GridEnvironment.py
+++ basic_environment/GridEnvironment.py
@@ -8,13 +8,14 @@ class CustomEnv(gym.Env):
     """Custom Environment that follows gym interface"""
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, grid_size):
+    def __init__(self, grid_size, img_size=(36, 36)):
         self.grid_size = grid_size
+        self.img_size = img_size
         super(CustomEnv, self).__init__()
         # Define action and observation space
         self.action_space = spaces.Discrete(4)
         # Example for using image as input:
-        self.observation_space = spaces.Box(low=0, high=255, shape=(36, 36, 3), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(img_size[0], img_size[1], 3), dtype=np.uint8)
 
     def reset(self):
         self.done = False
@@ -47,14 +48,15 @@ class CustomEnv(gym.Env):
             self.agent_position[0] = min(self.grid_size[0] - 1, self.agent_position[0] + 1)
 
         if self.agent_position == self.goal_position:
-            # reward the agent for reaching the goal
-            # self.reward = 100
             self.done = True
-        else:
-            distance = np.sqrt((self.agent_position[0] - self.goal_position[0]) ** 2 + (
-                    self.agent_position[1] - self.goal_position[1]) ** 2)
 
-            self.reward = - distance
+        # define reward as the decrease in distance to the goal
+        old_dist = abs(self.old_agent_position[-1][0] - self.goal_position[0]) + abs(
+            self.old_agent_position[-1][1] - self.goal_position[1])
+        new_dist = abs(self.agent_position[0] - self.goal_position[0]) + abs(
+            self.agent_position[1] - self.goal_position[1])
+        self.reward = old_dist - new_dist
+
 
         # define observation
         observation = self.getImg()
@@ -62,35 +64,32 @@ class CustomEnv(gym.Env):
         return np.array(observation, dtype=np.uint8), self.reward, self.done, {}
 
     def render(self, mode='human'):
-
-        cv2.imshow('Grid', self.getImg())
-        cv2.waitKey(1)  # waits for 1 millisecond
+        # Render the environment to the screen
+        img = self.getImg()
+        cv2.imshow('image', img)
+        cv2.waitKey(1)
 
     def close(self):
         cv2.destroyAllWindows()
 
     def getImg(self):
-        block_color = (255, 255, 255)  # white color in RGB
-        goal_color = (0, 255, 0)  # green color in RGB
-        old_position_color = (100, 100, 100)  # gray color in RGB
+        block_color = (255, 255, 255)
+        goal_color = (0, 255, 0)
+        old_position_color = (100, 100, 100)
 
         img = np.zeros((self.grid_size[0], self.grid_size[1], 3), dtype=np.uint8)
 
-        # # draw the old agent positions in gray
-        # for pos in self.old_agent_position:
-        #     img[pos[1]:(pos[1] + 1), pos[0]:(pos[0] + 1)] = old_position_color
+        # draw the old agent positions in gray
+        for old_position in self.old_agent_position:
+            img[old_position[1]:(old_position[1] + 1), old_position[0]:(old_position[0] + 1)] = old_position_color
 
-        # draw the active block (agent)
-        img[self.agent_position[1]:(self.agent_position[1] + 1),
-        self.agent_position[0] :(self.agent_position[0] + 1)] = block_color
+        # draw the agent position in white
+        img[self.agent_position[1]:(self.agent_position[1] + 1), self.agent_position[0]:(self.agent_position[0] + 1)] = block_color
 
-        # draw the goal block
-        img[self.goal_position[1]:(self.goal_position[1] + 1),
-        self.goal_position[0]:(self.goal_position[0] + 1)] = goal_color
+        # draw the goal position in green
+        img[self.goal_position[1]:(self.goal_position[1] + 1), self.goal_position[0]:(self.goal_position[0] + 1)] = goal_color
 
-        # scale the grid to 64x64 pixels
-        img = cv2.resize(img, (36, 36), interpolation=cv2.INTER_NEAREST)
-
-
+        # scale the grid to 36x36 pixels
+        img = cv2.resize(img, self.img_size, interpolation=cv2.INTER_NEAREST)
 
         return img
