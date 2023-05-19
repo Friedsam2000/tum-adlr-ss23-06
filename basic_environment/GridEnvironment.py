67247dac4adf2c3b64c28ad5ae7736d1843f1d1a
samuel <samuelzeitler2000@gmail.com>
2023-05-19 13:03:30 +0200

    added timeout 4 times manhatten distance with -1 reward

--- basic_environment/GridEnvironment.py
+++ basic_environment/GridEnvironment.py
@@ -13,7 +13,8 @@ class CustomEnv(gym.Env):
     def __init__(self, grid_size, img_size=(36, 36), render_size=(360, 360), draw_num_old_agent_pos=0):
 
         # assert that the grid size is smaller than the image size
-        assert grid_size[0] <= img_size[0] and grid_size[1] <= img_size[1], "The grid size must be smaller than the image size"
+        assert grid_size[0] <= img_size[0] and grid_size[1] <= img_size[
+            1], "The grid size must be smaller than the image size"
 
         self.draw_num_old_agent_pos = draw_num_old_agent_pos
         self.grid_size = grid_size
@@ -35,10 +36,15 @@ class CustomEnv(gym.Env):
         # old agent positions
         self.old_agent_position = deque(maxlen=self.draw_num_old_agent_pos)
         for i in range(self.draw_num_old_agent_pos):
-            self.old_agent_position.append([-1, -1]) # -1 means no position
-            
+            self.old_agent_position.append([-1, -1])  # -1 means no position
+
         # define last distance to goal
         self.old_dist = np.linalg.norm(np.array(self.agent_position) - np.array(self.goal_position))
+
+        # define step counter and timeout
+        self.steps = 0
+        self.timeout = 4 * (abs(self.agent_position[0] - self.goal_position[0]) + abs(
+            self.agent_position[1] - self.goal_position[1]))
 
         # define observation
         observation = self.getImg()
@@ -48,6 +54,7 @@ class CustomEnv(gym.Env):
     def step(self, action):
 
         self.reward = 0
+        self.steps += 1
 
         # save old agent position
         self.old_agent_position.append(self.agent_position.copy())
@@ -61,34 +68,38 @@ class CustomEnv(gym.Env):
         elif action == 3:  # right
             self.agent_position[0] = min(self.grid_size[0] - 1, self.agent_position[0] + 1)
 
-
         # check if the agent is at the goal position
         if self.agent_position == self.goal_position:
             self.reward += 10
             self.done = True
         else:
-            # define new distance to goal
-            new_dist = np.linalg.norm(np.array(self.agent_position) - np.array(self.goal_position))
-
-            # if the agent is moving towards the goal, give a positive reward
-            if new_dist < self.old_dist:
-                self.reward += 1
+            # check if timeout
+            if self.steps >= self.timeout:
+                self.reward -= 1
+                self.done = True
             else:
-                self.reward += -1
-    
-            # if the agent is moving against a wall, give a negative reward
-            if self.old_dist == new_dist:
-                self.reward += -0.4
-    
-            # any move is a negative reward
-            self.reward += -0.1
+                # define new distance to goal
+                new_dist = np.linalg.norm(np.array(self.agent_position) - np.array(self.goal_position))
 
-            # punish the agent for revisiting old positions
-            if self.agent_position in self.old_agent_position:
-                self.reward += -0.3
-            
-            # set the new distance to the old distance
-            self.old_dist = new_dist
+                # if the agent is moving towards the goal, give a positive reward
+                if new_dist < self.old_dist:
+                    self.reward += 1
+                else:
+                    self.reward += -1
+
+                # if the agent is moving against a wall, give a negative reward
+                if self.old_dist == new_dist:
+                    self.reward += -0.4
+
+                # any move is a negative reward
+                self.reward += -0.1
+
+                # punish the agent for revisiting old positions
+                if self.agent_position in self.old_agent_position:
+                    self.reward += -0.3
+
+                # set the new distance to the old distance
+                self.old_dist = new_dist
 
         # define observation
         observation = self.getImg()
@@ -100,7 +111,6 @@ class CustomEnv(gym.Env):
         img = cv2.resize(self.getImg(), self.render_size, interpolation=cv2.INTER_NEAREST)
         cv2.imshow('image', img)
         cv2.waitKey(100)
-
 
     def close(self):
         cv2.destroyAllWindows()
@@ -117,10 +127,12 @@ class CustomEnv(gym.Env):
             img[old_position[1]:(old_position[1] + 1), old_position[0]:(old_position[0] + 1)] = old_position_color
 
         # draw the agent position in white
-        img[self.agent_position[1]:(self.agent_position[1] + 1), self.agent_position[0]:(self.agent_position[0] + 1)] = block_color
+        img[self.agent_position[1]:(self.agent_position[1] + 1),
+        self.agent_position[0]:(self.agent_position[0] + 1)] = block_color
 
         # draw the goal position in green
-        img[self.goal_position[1]:(self.goal_position[1] + 1), self.goal_position[0]:(self.goal_position[0] + 1)] = goal_color
+        img[self.goal_position[1]:(self.goal_position[1] + 1),
+        self.goal_position[0]:(self.goal_position[0] + 1)] = goal_color
 
         # scale the grid to img_size
         img = cv2.resize(img, self.img_size, interpolation=cv2.INTER_NEAREST)
