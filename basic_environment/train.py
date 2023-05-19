9ec513e0b217075c66f3664495f32380c7a68d6e
samuel <samuelzeitler2000@gmail.com>
2023-05-19 15:59:51 +0200

    reset n_epchs and n_steps

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     logs_folders = os.listdir("logs")
 
     # Initialize PPO agent with CNN policy
-    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=2048, n_epochs=5)
+    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device)
 
     # Train agent
     TIMESTEPS_PER_SAVE = 20000
