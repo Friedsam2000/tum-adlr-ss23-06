79770b5fdb392e4cb6a6782b5bb56fc7c71c9a97
samuel <samuelzeitler2000@gmail.com>
2023-05-21 20:31:47 +0200

    high learning rate, default n_steps

--- basic_environment/scripts/test_trained.py
+++ basic_environment/scripts/test_trained.py
@@ -14,7 +14,7 @@ storage_client = google.cloud.storage.Client()
 bucket = storage_client.get_bucket(bucket_name)
 
 # Get all model filenames from the bucket
-PPO_Iteration = "PPO_13_0"
+PPO_Iteration = "PPO_18_0"
 blobs = bucket.list_blobs(prefix=f"basic_environment/models/{PPO_Iteration}")
 model_filenames = []
 for blob in blobs:
@@ -42,14 +42,14 @@ model = PPO.load(f"models_from_bucket/" + model_filename.split("/")[-1], custom_
 print(f"Loaded {model_filename} from models_from_bucket directory")
 
 # Create the environment
-env = CustomEnv(grid_size=(8, 8))
+env = CustomEnv(grid_size=(16, 16))
 
 # Test the model
 obs = env.reset()
 goals_reached = 0
 obstacles_hit = 0
 episodes = 0
-while episodes < 1000:
+while episodes < 100:
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
 
