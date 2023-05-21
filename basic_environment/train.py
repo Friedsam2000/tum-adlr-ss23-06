9daab340b15129b9dd2f0659063924311ea59b85
samuel <samuelzeitler2000@gmail.com>
2023-05-21 23:04:34 +0200

    small n_steps, high learning rate

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     logs_folders = os.listdir("logs")
 
     # Initialize PPO agent with CNN policy
-    n_steps = 1024
+    n_steps = 256
     model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=n_steps, learning_rate=0.003)
 
     # create the folder for the model
