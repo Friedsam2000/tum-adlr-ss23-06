59f9e60ed5463dd750fb5eaf4f3ced1422c7cb0f
samuel <samuelzeitler2000@gmail.com>
2023-05-21 21:21:49 +0200

    very small learning rate

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
 
     # Initialize PPO agent with CNN policy
     n_steps = 128
-    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=n_steps, learning_rate=0.00003)
+    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=n_steps, learning_rate=0.0000003)
 
     # create the folder for the model
     if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
