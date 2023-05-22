cad7a8a2876ae070b164507779a7e1f9d1f4fb39
samuel <samuelzeitler2000@gmail.com>
2023-05-22 11:26:20 +0200

    added continue_train on a ModifiedGridEnvironment

---
+++ basic_environment/ModifiedGridEnvironment.py
@@ -0,0 +1,186 @@
+from collections import deque
+import numpy as np
+import cv2
+import gym
+from gym import spaces
+from CheckGoalReachable import a_star_search
+
+
+class ModifiedCustomEnv(gym.Env):
+    """Custom Environment that follows gym interface"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, grid_size, img_size=(48, 48), render_size=(480, 480), num_last_agent_pos=2):
+
+        # assert that the grid size is smaller than the image size
+        assert grid_size[0] <= img_size[0] and grid_size[1] <= img_size[
+            1], "The grid size must be smaller than the image size or important information will be lost."
+
+        # assert that render size and image size are multiples of the grid size
+        assert img_size[0] % grid_size[0] == 0 and img_size[1] % grid_size[1] == 0, \
+            "The image size must be a multiple of the grid size or important information will be lost."
+        assert render_size[0] % grid_size[0] == 0 and render_size[1] % grid_size[1] == 0, \
+            "The render size must be a multiple of the grid size or important information will be lost."
+
+        self.num_last_agent_pos = num_last_agent_pos
+        self.grid_size = grid_size
+        self.img_size = img_size
+        self.render_size = render_size
+        super(ModifiedCustomEnv, self).__init__()
+        # Define action and observation space
+        self.action_space = spaces.Discrete(4)
+        # Example for using image as input:
+        self.observation_space = spaces.Box(low=0, high=255, shape=(img_size[0], img_size[1], 3), dtype=np.uint8)
+
+    def reset(self):
+        self.done = False
+
+        # create a deque to store the last agent positions
+        self.last_agent_positions = deque(maxlen=self.num_last_agent_pos)
+        # create history of agent positions and set to -1
+        for i in range(self.num_last_agent_pos):
+            self.last_agent_positions.append([-1, -1])
+
+        # set the agent position to top left corner
+        self.agent_position = [0 + self.grid_size[0]//5, 0 + self.grid_size[1]//5]
+
+        # set the goal position to bottom right corner
+        self.goal_position = [self.grid_size[0] - self.grid_size[0]//5, self.grid_size[1] - self.grid_size[1]//5]
+
+        # spawn random obstacles not on goal or agent position
+        goal_is_reachable = False
+        while not goal_is_reachable:
+            self.obstacle_positions = []  # Clear the old obstacles
+            for i in range(self.grid_size[0]*2): # add obstacles to the grid equal to the side length of the grid
+                obstacle_position = [np.random.randint(0, self.grid_size[0]), np.random.randint(0, self.grid_size[1])]
+                while obstacle_position == self.agent_position or obstacle_position == self.goal_position:
+                    obstacle_position = [np.random.randint(0, self.grid_size[0]),
+                                         np.random.randint(0, self.grid_size[1])]
+                self.obstacle_positions.append(obstacle_position)
+            goal_is_reachable = self.check_goal_reachable(self.goal_position, self.agent_position,
+                                                          self.obstacle_positions)
+
+        # define last distance to goal
+        self.old_dist = np.linalg.norm(np.array(self.agent_position) - np.array(self.goal_position))
+
+        # define step counter and timeout as 4 times the manhattan distance between agent and goal
+        self.steps = 0
+        self.timeout = 6 * (abs(self.agent_position[0] - self.goal_position[0]) + abs(
+            self.agent_position[1] - self.goal_position[1])) + 1
+
+        # define observation
+        observation = self.getImg()
+
+        return np.array(observation, dtype=np.uint8)
+
+    def step(self, action):
+
+        self.reward = 0
+        self.steps += 1
+
+        # append the current agent position to the last agent positions
+        self.last_agent_positions.append(self.agent_position.copy())
+
+        if action == 0:  # up
+            self.agent_position[1] = max(0, self.agent_position[1] - 1)
+        elif action == 1:  # down
+            self.agent_position[1] = min(self.grid_size[1] - 1, self.agent_position[1] + 1)
+        elif action == 2:  # left
+            self.agent_position[0] = max(0, self.agent_position[0] - 1)
+        elif action == 3:  # right
+            self.agent_position[0] = min(self.grid_size[0] - 1, self.agent_position[0] + 1)
+
+        # check if the agent hit an obstacle
+        if self.agent_position in self.obstacle_positions:
+            self.reward = -1
+            self.done = True
+            return np.array(self.getImg(), dtype=np.uint8), self.reward, self.done, {"goal": False, "obstacle": True}
+
+        # check if the agent is at the goal position
+        if self.agent_position == self.goal_position:
+            self.reward = 1
+
+            self.done = True
+            # return info that goal was reached
+            return np.array(self.getImg(), dtype=np.uint8), self.reward, self.done, {"goal": True, "obstacle": False}
+
+
+
+        # check if timeout
+        if self.steps >= self.timeout:
+            self.reward = -1
+            self.done = True
+            return np.array(self.getImg(), dtype=np.uint8), self.reward, self.done, {"goal": False, "obstacle": False}
+
+        # define new distance to goal
+        new_dist = np.linalg.norm(np.array(self.agent_position) - np.array(self.goal_position))
+
+        # if the agent is moving towards the goal, give a positive reward, if not, give a negative reward
+        if new_dist < self.old_dist:
+            self.reward = 0.025
+        elif new_dist == self.old_dist: #wall hit
+            self.reward = -0.05
+        else:
+            self.reward = -0.025
+
+        # punish the agent for revisiting old positions
+        if self.agent_position in self.last_agent_positions:
+            self.reward -= 0.025
+
+
+        # set the new distance to the old distance
+        self.old_dist = new_dist
+
+        return np.array(self.getImg(), dtype=np.uint8), self.reward, self.done, {}
+
+    def render(self, mode='human'):
+        # Render the environment to the screen
+        img = cv2.resize(self.getImg(), self.render_size, interpolation=cv2.INTER_NEAREST)
+        cv2.imshow('image', img)
+        cv2.waitKey(200)
+
+    def close(self):
+        cv2.destroyAllWindows()
+
+    def getImg(self):
+        block_color = (255, 255, 255)
+        goal_color = (0, 255, 0)
+        obstacle_color = (0, 0, 255)
+        old_agent_color = (255, 192, 203)
+
+
+        img = np.zeros((self.grid_size[0], self.grid_size[1], 3), dtype=np.uint8)
+
+        # draw the last agent positions in light pink if not -1
+        for position in self.last_agent_positions:
+            if position != [-1, -1]:
+                img[position[1]:(position[1] + 1),
+                position[0]:(position[0] + 1)] = old_agent_color
+
+        # draw the agent position in white
+        img[self.agent_position[1]:(self.agent_position[1] + 1),
+        self.agent_position[0]:(self.agent_position[0] + 1)] = block_color
+
+        # draw the goal position in green
+        img[self.goal_position[1]:(self.goal_position[1] + 1),
+        self.goal_position[0]:(self.goal_position[0] + 1)] = goal_color
+
+        # draw the obstacle positions in red
+        for obstacle_position in self.obstacle_positions:
+            img[obstacle_position[1]:(obstacle_position[1] + 1),
+            obstacle_position[0]:(obstacle_position[0] + 1)] = obstacle_color
+
+        # scale the grid to img_size
+        img = cv2.resize(img, self.img_size, interpolation=cv2.INTER_NEAREST)
+
+        return img
+
+    def check_goal_reachable(self, goal_position, agent_position, obstacle_positions):
+        obstacle_positions = set(tuple(pos) for pos in obstacle_positions)
+        check_goal_reachable = a_star_search(agent_position, goal_position, obstacle_positions, self.grid_size)
+        if not check_goal_reachable:
+            print("Goal is not reachable, resetting environment")
+        # if check_goal_reachable:
+            # print("Goal is reachable")
+        return check_goal_reachable
+
---
+++ basic_environment/continue_train.py
@@ -0,0 +1,115 @@
+import numpy as np
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecMonitor
+from stable_baselines3 import PPO
+from ModifiedGridEnvironment import ModifiedCustomEnv
+import os
+import torch
+from google.cloud import storage
+import shutil
+
+
+
+def make_env(grid_size, rank):
+    def _init():
+        env = ModifiedCustomEnv(grid_size=grid_size)
+        return env
+
+    return _init
+
+
+if __name__ == "__main__":
+
+    # Set up the GPU or use the CPU
+    print("GPU is available: ")
+    print(torch.cuda.is_available())
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # Set up the bucket (google cloud storage)
+    # Define the bucket name
+    bucket_name = 'adlr_bucket'
+    # Initialize a storage client
+    storage_client = storage.Client()
+    # Get the bucket object
+    bucket = storage_client.get_bucket(bucket_name)
+
+    num_cpu = 16  # Number of processes to use
+    grid_size = (16, 16)
+
+    # Create the vectorized environment
+    env = SubprocVecEnv([make_env(grid_size, i) for i in range(num_cpu)])
+    # add a monitor wrapper
+    env = VecMonitor(env)
+
+    # Create logs if not existing
+    if not os.path.exists("logs_modified"):
+        os.makedirs("logs_modified")
+
+    # Create models if not existing
+    if not os.path.exists("models_modified"):
+        os.makedirs("models_modified")
+
+    # Check how many folders are in logs
+    logs_folders = os.listdir("logs_modified")
+
+    # Get all model filenames from the bucket
+    PPO_Iteration = "PPO_0_0"
+    blobs = bucket.list_blobs(prefix=f"basic_environment/models/{PPO_Iteration}")
+    model_filenames = []
+    for blob in blobs:
+        model_filenames.append(blob.name)
+
+    # Integer sort the model filenames
+    model_filenames = sorted(model_filenames, key=lambda x: int(x.split("/")[-1].split(".")[0]))
+
+    # Empty or create the models_from_bucket directory using shutil
+    if os.path.exists("models_to_modify"):
+        shutil.rmtree("models_to_modify")
+    os.makedirs("models_to_modify")
+
+    # Download the model with the highest number of steps in the models_from_bucket directory
+    model_filename = model_filenames[-1]
+    blob = bucket.blob(model_filename)
+    blob.download_to_filename(f"models_to_modify/" + model_filename.split("/")[-1])
+    print(f"Downloaded {model_filename} from bucket {bucket_name} to models_to_modify directory")
+
+    # Load the model
+    custom_objects = {"lr_schedule": lambda _: 0.0, "clip_range": lambda _: 0.0}
+    env.reset()
+    model = PPO.load(f"models_to_modify/" + model_filename.split("/")[-1], env=env, custom_objects=custom_objects, verbose=1)
+    # set new tensorboard log directory
+    model.tensorboard_log = "logs_modified"
+    print(f"Loaded {model_filename} from models_to_modify directory")
+
+    # create the folder for the model
+    if not os.path.exists(f"models_modified/PPO_{len(logs_folders)}"):
+        os.makedirs(f"models_modified/PPO_{len(logs_folders)}")
+
+    best_reward = -np.inf
+
+    # Continue training
+    TIMESTEPS_PER_SAVE = model.n_steps * num_cpu
+    MAX_TIMESTEPS = 3000000
+    while model.num_timesteps < MAX_TIMESTEPS:
+        model.learn(total_timesteps=TIMESTEPS_PER_SAVE, reset_num_timesteps=False)
+
+        # get the mean reward of the last 100 episodes
+        reward_mean = np.mean([ep['r'] for ep in list(model.ep_info_buffer)[-100:]])
+
+        # if the reward mean is better than the best reward, save the model
+        if reward_mean > best_reward:
+            best_reward = reward_mean
+            print(f"Saving model with new best reward mean {reward_mean}")
+            model.save(f"models_modified/PPO_{len(logs_folders)}/{model.num_timesteps}")
+
+            # upload the model to the bucket
+            blob = bucket.blob(f"basic_environment/models/PPO_{len(logs_folders)}_0_modified/{model.num_timesteps}.zip")
+            blob.upload_from_filename(f"models_modified/PPO_{len(logs_folders)}/{model.num_timesteps}.zip")
+            print(f"Uploaded model {model.num_timesteps}.zip to bucket")
+
+        # get the latest log file
+        logs = os.listdir(f"logs_modified/PPO_{len(logs_folders)}")
+        logs.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+        latest_log = logs[-1]
+        # upload the new log file to the bucket
+        blob = bucket.blob(f"basic_environment/logs/PPO_{len(logs_folders)}_0_modified/{latest_log}")
+        blob.upload_from_filename(f"logs_modified/PPO_{len(logs_folders)}/{latest_log}")
