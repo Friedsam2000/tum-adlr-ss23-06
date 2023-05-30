c47db8cd07173898a99888b8e2b11a9e0787487d
samuel <samuelzeitler2000@gmail.com>
2023-05-30 18:07:56 +0200

    high batch isze

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
 
     # Initialize PPO agent with CNN policy
     n_steps = 512
-    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=n_steps, batch_size=n_steps*num_cpu*4)
+    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=n_steps, batch_size=n_steps*num_cpu*16)
 
     # create the folder for the model
     if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
