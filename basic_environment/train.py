269e5a60f86a98dfeb2102a435df0548adfa3147
samuel <samuelzeitler2000@gmail.com>
2023-05-30 21:09:19 +0200

    provided params from website

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
 
     # Initialize PPO agent with CNN policy
     n_steps = 100
-    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=n_steps, n_epochs=0, learning_rate=0.00025, gamma=0.98, gae_lambda=1.0, ent_coef=0.00025, batch_size=100)
+    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=n_steps, learning_rate=0.00025, gamma=0.98, gae_lambda=1.0, ent_coef=0.00025, batch_size=100)
 
     # create the folder for the model
     if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
