eff997c16651056ac638b606630ab034e755cfb4
samuel <samuelzeitler2000@gmail.com>
2023-05-31 00:15:35 +0200

    lower n_steps

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -51,8 +51,8 @@ if __name__ == "__main__":
     logs_folders = os.listdir("logs")
 
     # Initialize PPO agent with CNN policy
-    n_steps = 128
-    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=n_steps)
+    n_steps = 64
+    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log="logs", device=device, n_steps=n_steps, learning_rate=1e-4)
 
     # create the folder for the model
     if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     best_reward = -np.inf
 
     # Train agent
-    TIMESTEPS_PER_SAVE = 16384*4
+    TIMESTEPS_PER_SAVE = 16384
     MAX_TIMESTEPS = 100000000
     while model.num_timesteps < MAX_TIMESTEPS:
         model.learn(total_timesteps=TIMESTEPS_PER_SAVE, reset_num_timesteps=False,
