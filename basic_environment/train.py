15540144add2a5b27a5421659eee58a84d589550
samuel <samuelzeitler2000@gmail.com>
2023-05-31 04:29:31 +0200

    n_steps increased

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -60,9 +60,7 @@ if __name__ == "__main__":
     )
 
     # Initialize PPO agent with CNN policy
-    n_steps = 1024
-    model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device,
-                n_steps=n_steps)
+    model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device)
 
     # create the folder for the model
     if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
