0955a1ae3e98382e9827a0cca5a169dea955be93
Matthias <matthias.kreiner29@gmail.com>
2023-06-22 15:02:55 +0200

    Updated venv to SB3 1.5.0

--- basic_environment/install.sh
+++ basic_environment/install.sh
@@ -9,7 +9,8 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Ensure that pip, setuptools, and wheel are up-to-date
-pip install --upgrade pip setuptools wheel
+pip install --upgrade wheel
+pip install setuptools==65.5.0 pip==21
 
 # Install Python dependencies
 pip install -r requirements.txt
