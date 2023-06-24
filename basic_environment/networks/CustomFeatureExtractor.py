7b442bd50937a99dcb0429f7b61bfc29bb6672cc
samuel <samuelzeitler2000@gmail.com>
2023-06-25 00:01:07 +0200

    newer version

--- basic_environment/networks/CustomFeatureExtractor.py
+++ basic_environment/networks/CustomFeatureExtractor.py
@@ -3,27 +3,35 @@ from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 import gym
 import torch
 
-
 class CustomFeatureExtractor(BaseFeaturesExtractor):
     def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 256):
         super(CustomFeatureExtractor, self).__init__(observation_space, features_dim)
 
         # Assume input has shape (48, 48, 3)
         self.cnn = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=2, stride=1, padding=1),
+            nn.Conv2d(3, 16, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(16, 32, kernel_size=2, stride=1, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
             nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
-            nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(64, 64, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
             nn.Flatten(),
         )
@@ -34,6 +42,9 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
 
         self.linear = nn.Sequential(
             nn.Linear(n_flatten, features_dim),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(features_dim, features_dim),
             nn.ReLU(),
         )
 
