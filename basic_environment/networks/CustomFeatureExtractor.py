126d7a778e4208b5fdd243df0bae165130c733c9
samuel <samuelzeitler2000@gmail.com>
2023-06-25 01:25:00 +0200

    Revert "even bigger"
    This reverts commit c8f6bb731bb492bc16e824a3bde31cefee610594.

--- basic_environment/networks/CustomFeatureExtractor.py
+++ basic_environment/networks/CustomFeatureExtractor.py
@@ -4,12 +4,18 @@ import gym
 import torch
 
 class CustomFeatureExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 1024):
+    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 512):
         super(CustomFeatureExtractor, self).__init__(observation_space, features_dim)
 
         # Assume input has shape (48, 48, 3)
         self.cnn = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=2, stride=1, padding=1),
+            nn.Conv2d(3, 32, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
@@ -21,20 +27,8 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
-            nn.Conv2d(128, 256, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
-
-            nn.Conv2d(256, 512, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
-
-            nn.Conv2d(512, 512, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(512),
+            nn.Conv2d(128, 128, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
@@ -48,6 +42,9 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
 
         self.linear = nn.Sequential(
             nn.Linear(n_flatten, features_dim),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(features_dim, features_dim),
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(features_dim, features_dim),
--- basic_environment/train.py
+++ basic_environment/train.py
@@ -56,7 +56,8 @@ if __name__ == "__main__":
     )
 
     # Initialize PPO agent with new policy architecture
-    model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device)
+    model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device,
+                n_steps=1024)
 
     # create the folder for the model
     if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
