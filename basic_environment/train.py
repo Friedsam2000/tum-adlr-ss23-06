2f4139f974ea07add171acb1d664b0986afdb204
samuel <samuelzeitler2000@gmail.com>
2023-06-24 18:41:13 +0200

    n_steps 1024

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     )
 
     # Initialize PPO agent with CNN policy
-    model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device, n_steps=2048*2)
+    model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device, n_steps=1024)
 
     # create the folder for the model
     if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
