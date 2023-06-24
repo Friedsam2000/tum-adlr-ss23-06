c8f6bb731bb492bc16e824a3bde31cefee610594
samuel <samuelzeitler2000@gmail.com>
2023-06-25 01:23:31 +0200

    even bigger

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -56,8 +56,7 @@ if __name__ == "__main__":
     )
 
     # Initialize PPO agent with new policy architecture
-    model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device,
-                n_steps=1024)
+    model = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device)
 
     # create the folder for the model
     if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
