f6649bfa877576ab55b2f95b18466512fab25d66
samuel <samuelzeitler2000@gmail.com>
2023-06-26 13:47:28 +0200

    test6

--- basic_environment/networks/CustomFeatureExtractor.py
+++ basic_environment/networks/CustomFeatureExtractor.py
@@ -10,25 +10,25 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
         # Assume input has shape (48, 48, 3)
         self.cnn = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
             nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(256),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
             nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
             nn.Conv2d(128, 256, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
