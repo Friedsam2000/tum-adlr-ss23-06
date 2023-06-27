5922cb500e2cfcf4a792828e79eaf0d5471cf357
samuel <samuelzeitler2000@gmail.com>
2023-06-27 23:43:01 +0200

    test3

--- basic_environment/networks/CustomFeatureExtractor.py
+++ basic_environment/networks/CustomFeatureExtractor.py
@@ -5,7 +5,7 @@ import torch
 
 
 class CustomFeatureExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 64):
+    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 128):
         super(CustomFeatureExtractor, self).__init__(observation_space, features_dim)
 
         # Assume input has shape (16, 16, 3*num_frames_to_stack)
@@ -26,13 +26,13 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
             nn.Conv2d(8, 16, kernel_size=2, stride=1, padding=1),
             # nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
             nn.Conv2d(16, 32, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(32),
+            # nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
             nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1),
