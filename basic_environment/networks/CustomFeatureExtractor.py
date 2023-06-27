18191824d1f785a262d455c714694a8581435747
samuel <samuelzeitler2000@gmail.com>
2023-06-27 23:46:19 +0200

    test4

--- basic_environment/networks/CustomFeatureExtractor.py
+++ basic_environment/networks/CustomFeatureExtractor.py
@@ -27,18 +27,18 @@ class CustomFeatureExtractor(BaseFeaturesExtractor):
             # nn.BatchNorm2d(16),
             nn.ReLU(),
             # nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
+            # nn.Dropout(0.1),
 
             nn.Conv2d(16, 32, kernel_size=2, stride=1, padding=1),
             # nn.BatchNorm2d(32),
             nn.ReLU(),
             # nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
+            # nn.Dropout(0.1),
 
             nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(64),
+            # nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
             nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1),
