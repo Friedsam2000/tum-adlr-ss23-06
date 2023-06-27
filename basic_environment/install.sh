7bce850a0aa85c8b38b965abfcf88eaa092fac3b
samuel <samuelzeitler2000@gmail.com>
2023-06-27 16:20:03 +0200

    updated install script

--- basic_environment/install.sh
+++ basic_environment/install.sh
@@ -11,6 +11,8 @@ source venv/bin/activate
 # Ensure that pip, setuptools, and wheel are up-to-date
 pip install --upgrade pip setuptools wheel
 
+# Install specific versions of PyTorch, torchvision, and torchaudio compatible with CUDA 11.3
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+
 # Install Python dependencies
 pip install -r requirements.txt
-
