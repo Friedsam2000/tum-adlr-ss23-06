ca81d0918add109b52be5f3fcb43b734e6c78ae5
samuel <samuelzeitler2000@gmail.com>
2023-06-28 19:37:47 +0200

    cannocical network

--- basic_environment/networks/CustomFeatureExtractor.py
+++ basic_environment/networks/CustomFeatureExtractor.py
@@ -5,34 +5,22 @@ import torch
 
 
 class CustomFeatureExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 256):
+    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 128):
         super(CustomFeatureExtractor, self).__init__(observation_space, features_dim)
 
         # Assume input has shape (img_size, img_size, 3*num_frames_to_stack)
         self.cnn = nn.Sequential(
-            nn.Conv2d(observation_space.shape[0], 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(observation_space.shape[0], 32, kernel_size=8, stride=4, padding=0),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
 
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
 
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
-
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
 
             nn.Flatten(),
         )
