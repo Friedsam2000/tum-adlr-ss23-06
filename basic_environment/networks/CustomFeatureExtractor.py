42b808d1730db230411f98e18f2a5bd8eacb02ce
samuel <samuelzeitler2000@gmail.com>
2023-06-28 03:24:34 +0200

    test 100

--- basic_environment/networks/CustomFeatureExtractor.py
+++ basic_environment/networks/CustomFeatureExtractor.py
@@ -5,36 +5,40 @@ import torch
 
 
 class CustomFeatureExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 128):
+    def __init__(self, observation_space: gym.spaces.Box, features_dim: int = 256):
         super(CustomFeatureExtractor, self).__init__(observation_space, features_dim)
 
-        # Assume input has shape (16, 16, 3*num_frames_to_stack)
+        # Assume input has shape (84, 84, 3*num_frames_to_stack)
         self.cnn = nn.Sequential(
-
-            nn.Conv2d(observation_space.shape[0], 8, kernel_size=1, stride=1, padding=0),
-            # nn.BatchNorm2d(8),
+            nn.Conv2d(observation_space.shape[0], 32, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            # nn.MaxPool2d(kernel_size=2, stride=2),
-            # nn.Dropout(0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
-            nn.Conv2d(8, 8, kernel_size=2, stride=1, padding=1),
-            # nn.BatchNorm2d(8),
-            nn.ReLU(),
-            # nn.MaxPool2d(kernel_size=2, stride=2),
-            # nn.Dropout(0.1),
-
-            nn.Conv2d(8, 64, kernel_size=2, stride=1, padding=1),
+            nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
-            nn.Conv2d(64, 256, kernel_size=2, stride=1, padding=1),
+            nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
+
+            nn.Conv2d(128, 256, kernel_size=2, stride=1, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
+            nn.Conv2d(256, 512, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
             nn.Flatten(),
         )
