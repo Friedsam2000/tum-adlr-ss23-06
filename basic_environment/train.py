ac84fa16af9b0dd72f3a88ca14f5196f3085ebe7
samuel <samuelzeitler2000@gmail.com>
2023-07-02 15:22:09 +0200

    DQN buffer size

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     )
 
     # Initialize PPO agent with new policy architecture
-    model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device, learning_rate=3e-5, )
+    model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device, learning_rate=3e-5, buffer_size=(1e5))
 
     # create the folder for the model
     if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
