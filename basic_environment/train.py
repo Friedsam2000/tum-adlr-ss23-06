ac98d29b7f4c9a8dc0acd66ea3bd31037455f3cb
samuel <samuelzeitler2000@gmail.com>
2023-07-02 19:08:29 +0200

    smaller buffer size

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -55,8 +55,8 @@ if __name__ == "__main__":
     )
 
     # Initialize DQN agent with new policy architecture
-    model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device,
-                learning_rate=3e-5, buffer_size=50000)
+    model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, device=device,
+                learning_rate=3e-5, buffer_size=25000)
 
     # Train agent
     model.learn(total_timesteps=int(1e5))
