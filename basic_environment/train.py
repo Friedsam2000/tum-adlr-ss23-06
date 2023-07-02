4b8431f571e77381a18434c160cf7807ea1fd211
samuel <samuelzeitler2000@gmail.com>
2023-07-02 21:50:48 +0200

    smaller learning rate

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     )
 
     # Initialize DQN agent with new policy architecture
-    model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device, learning_rate=3e-5, buffer_size=20000, learning_starts=20000)
+    model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device, learning_rate=3e-6, buffer_size=20000, learning_starts=20000)
 
 
 
