4855078d36a0d5ca733a5711760b282519f8cde0
samuel <samuelzeitler2000@gmail.com>
2023-07-02 15:34:30 +0200

    DQN buffer size

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -58,40 +58,8 @@ if __name__ == "__main__":
     model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device,
                 learning_rate=3e-5, buffer_size=int(1e5))
 
-    # create the folder for the model
-    if not os.path.exists(f"models/PPO_{len(logs_folders)}_0"):
-        os.makedirs(f"models/PPO_{len(logs_folders)}_0")
+    # Train the agent
+    model.learn(total_timesteps=int(1e6))
 
-    best_reward = -np.inf
-
-    # Train agent
-    TIMESTEPS_PER_SAVE = 16384
-    MAX_TIMESTEPS = 100000000
-    while model.num_timesteps < MAX_TIMESTEPS:
-        model.learn(total_timesteps=TIMESTEPS_PER_SAVE, reset_num_timesteps=False,
-                    tb_log_name=f"PPO_{len(logs_folders)}")
-
-        # get the mean reward of the last 100 episodes
-        reward_mean = np.mean([ep['r'] for ep in list(model.ep_info_buffer)[-100:]])
-
-        # if the reward mean is better than the best reward, save the model
-        if reward_mean > best_reward:
-            best_reward = reward_mean
-            print(f"Saving model with new best reward mean {reward_mean}")
-            model.save(f"models/PPO_{len(logs_folders)}_0/{model.num_timesteps}")
-
-            # upload the model to the bucket
-            blob = bucket.blob(f"basic_environment/models/PPO_{len(logs_folders)}_0/{model.num_timesteps}.zip")
-            blob.upload_from_filename(f"models/PPO_{len(logs_folders)}_0/{model.num_timesteps}.zip")
-            print(f"Uploaded model {model.num_timesteps}.zip to bucket")
-
-            # delete the model locally
-            os.remove(f"models/PPO_{len(logs_folders)}_0/{model.num_timesteps}.zip")
-
-        # get the latest log file
-        logs = os.listdir(f"logs/PPO_{len(logs_folders)}_0")
-        logs.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-        latest_log = logs[-1]
-        # upload the new log file to the bucket
-        blob = bucket.blob(f"basic_environment/logs/PPO_{len(logs_folders)}_0/{latest_log}")
-        blob.upload_from_filename(f"logs/PPO_{len(logs_folders)}_0/{latest_log}")
+    # Save the agent
+    model.save("models/dqn")
