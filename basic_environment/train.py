eb7dfb2481eafbf3324376d3e5ae36911559d094
samuel <samuelzeitler2000@gmail.com>
2023-07-02 18:51:17 +0200

    smaller buffer size

--- basic_environment/train.py
+++ basic_environment/train.py
@@ -8,6 +8,13 @@ import torch
 from networks.CustomFeatureExtractor import CustomFeatureExtractor
 
 
+def make_env(rank):
+    def _init():
+        env = CustomEnv()
+        return env
+
+    return _init
+
 
 if __name__ == "__main__":
 
@@ -26,8 +33,10 @@ if __name__ == "__main__":
 
     num_cpu = 8  # Number of processes to use
 
-    # Create the environment
-    env = CustomEnv()
+    # Create the vectorized environment
+    env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
+    # add a monitor wrapper
+    env = VecMonitor(env)
 
     # Create logs if not existing
     if not os.path.exists("logs"):
@@ -45,12 +54,28 @@ if __name__ == "__main__":
         features_extractor_class=CustomFeatureExtractor,
     )
 
-    # Initialize PPO agent with new policy architecture
-    model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device, buffer_size=50000)
+    # Initialize DQN agent with new policy architecture
+    model = DQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="logs", device=device,
+                learning_rate=3e-5, buffer_size=1024)
 
-    # Train the agent
-    model.learn(total_timesteps=int(40000))
-
+    # Train agent
+    model.learn(total_timesteps=int(1e6))
 
     # Save the agent
-    model.save("models/dqn")
+    model.save("models/DQN")
+
+    # upload the model to the bucket
+    blob = bucket.blob(f"basic_environment/models/DQN.zip")
+    blob.upload_from_filename(f"models/DQN.zip")
+    print(f"Uploaded model DQN.zip to bucket")
+    # delete the model locally
+    os.remove(f"models/DQN.zip")
+
+    # get the latest log file
+    logs = os.listdir(f"logs/DQN")
+    logs.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+    latest_log = logs[-1]
+    # upload the new log file to the bucket
+    blob = bucket.blob(f"basic_environment/logs/DQN/{latest_log}")
+    blob.upload_from_filename(f"logs/DQN/{latest_log}")
+
