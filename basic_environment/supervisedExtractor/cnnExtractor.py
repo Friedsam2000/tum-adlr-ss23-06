4ec952a3f7ffb5332f58b5d5a823076537efe055
samuel <samuelzeitler2000@gmail.com>
2023-08-03 21:29:18 +0200

    kernel size 3

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -1,47 +1,39 @@
 import torch
 import torch.nn as nn
 
-
 class CNNExtractor(nn.Module):
     def __init__(self):
         super(CNNExtractor, self).__init__()
 
         self.features = nn.Sequential(
-
             # size is 96x96x3
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.AvgPool2d(kernel_size=3, stride=2),
 
-
-
-            # size is 48x48x32
+            # size is 47x47x32
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.AvgPool2d(kernel_size=3, stride=2),
 
-            # size is 24x24x64
+            # size is 23x23x64
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=3, stride=2),
 
-            # size is 12x12x128
+            # size is 23x23x128
             nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=3, stride=2),
 
-            # size is 6x6x128
+            # size is 23x23x64
             nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=3, stride=2),
 
-            # size is 3x3x64
+            # size is 23x23x32
         )
-
 
         self.classifier = nn.Sequential(
             # 4 fully connected layers
-            nn.Linear(32 * 3 * 3, 512),
+            nn.Linear(32 * 23*23, 512), # Here, you need to match the flattened size
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(512, 256),
@@ -51,7 +43,6 @@ class CNNExtractor(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(128, 49),
-
         )
 
     def forward(self, x):
