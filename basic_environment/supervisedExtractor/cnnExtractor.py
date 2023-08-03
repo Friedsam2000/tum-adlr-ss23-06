bc2126c781c4473476b224ac1af7cba2717f0104
samuel <samuelzeitler2000@gmail.com>
2023-08-03 21:24:36 +0200

    kernel size 3

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -9,29 +9,31 @@ class CNNExtractor(nn.Module):
         self.features = nn.Sequential(
 
             # size is 96x96x3
-            nn.Conv2d(3, 32, kernel_size=2, stride=1, padding=1),
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(kernel_size=3, stride=2),
 
-            # size is 48x48x16
-            nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1),
-            nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
 
-            # size is 24x24x32
-            nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1),
-            nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
 
-            # size is 12x12x64
-            nn.Conv2d(128, 64, kernel_size=2, stride=1, padding=1),
+            # size is 48x48x32
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(kernel_size=3, stride=2),
+
+            # size is 24x24x64
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=3, stride=2),
+
+            # size is 12x12x128
+            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=3, stride=2),
 
             # size is 6x6x128
-            nn.Conv2d(64, 32, kernel_size=2, stride=1, padding=1),
+            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(kernel_size=3, stride=2),
 
             # size is 3x3x64
         )
