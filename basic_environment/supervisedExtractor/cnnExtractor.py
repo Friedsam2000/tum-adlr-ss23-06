76b66a53dea6b5ca8ff4f3cfa16ca20ef72ebc5b
samuel <samuelzeitler2000@gmail.com>
2023-08-03 17:02:39 +0200

    no batch norm

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -10,33 +10,27 @@ class CNNExtractor(nn.Module):
 
             #size is 96x96x3
             nn.Conv2d(3, 16, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
             #size is 48x48x16
             nn.Conv2d(16, 32, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
             #size is 24x24x32
             nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
             #size is 12x12x64
             nn.Conv2d(64, 32, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
-
-
 
 
         )
