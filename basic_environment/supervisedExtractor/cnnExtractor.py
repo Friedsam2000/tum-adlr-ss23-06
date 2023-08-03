4109a79a06965f2f5ce04bc51d834676cfbf4a0c
samuel <samuelzeitler2000@gmail.com>
2023-08-03 13:58:08 +0200

    comments on CNN

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -6,32 +6,39 @@ class CNNExtractor(nn.Module):
         super(CNNExtractor, self).__init__()
 
         self.fc_obstacle = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, padding=1),
+
+            nn.Conv2d(3, 16, kernel_size=3, padding=1), # Output size: 96x96x16
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.Dropout(0.5), # Added Dropout here
+
             nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1), # Output size: 48x48x32
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Dropout(0.5), # Added Dropout here
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1), # Output size: 48x48x64
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Dropout(0.5), # Added Dropout here
+
             nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1), # Output size: 24x24x128
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Dropout(0.5), # Added Dropout here
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1), # Output size: 24x24x256
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Dropout(0.5), # Added Dropout here
-            nn.Conv2d(256, 128, kernel_size=3, stride=2, padding=1), # Output size: 8x8x128
+
+            nn.Conv2d(256, 128, kernel_size=3, stride=2, padding=1), # Output size: 12x12x128
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Dropout(0.5), # Added Dropout here
-            nn.Conv2d(128, 1, kernel_size=2),  # Output size: 7x7x1
-            nn.AdaptiveAvgPool2d((7, 7))
+
+            nn.Conv2d(128, 1, kernel_size=2),  # Output size: 11x11x1
+            nn.AdaptiveAvgPool2d((7, 7)) # Output size: 7x7x1
         )
 
     def forward(self, x):
