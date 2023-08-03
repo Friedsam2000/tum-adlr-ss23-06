4a23ff07111d2cd368f502511f7e619cde448448
samuel <samuelzeitler2000@gmail.com>
2023-08-03 16:44:26 +0200

    simple cnn fc

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -7,58 +7,56 @@ class CNNExtractor(nn.Module):
         super(CNNExtractor, self).__init__()
 
         self.features = nn.Sequential(
-            # Block 1 (Output size: 64x224x224)
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # Output size: 64x112x112
 
-            # Block 2 (Output size: 128x112x112)
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # Output size: 128x56x56
+            #size is 96x96x3
+            nn.Conv2d(3, 16, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
-            # Block 3 (Output size: 256x56x56)
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # Output size: 256x28x28
+            #size is 48x48x16
+            nn.Conv2d(16, 32, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
-            # Block 4 (Output size: 512x28x28)
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # Output size: 512x14x14
+            #size is 24x24x32
+            nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
-            # Block 5 (Output size: 512x14x14)
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)  # Output size: 512x7x7
+            #size is 12x12x64
+            nn.Conv2d(64, 32, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
+
+
+
+
         )
 
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))  # Output size: 512x7x7
 
         self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 49)  # Output size: 7x7
+            # 3 fully connected layers
+            nn.Linear(32 * 6 * 6, 512),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 49),
+
+
         )
 
     def forward(self, x):
         x = self.features(x)
-        x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
-        x = x.view(x.size(0), 7, 7)  # Reshaping to 7x7
         return x
