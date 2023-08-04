598886e3e9f74edbe06d5e154b3c966bd6ed01db
samuel <samuelzeitler2000@gmail.com>
2023-08-04 15:08:55 +0200

    one less fc layer

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -33,10 +33,7 @@ class CNNExtractor(nn.Module):
 
         self.classifier = nn.Sequential(
             # 4 fully connected layers
-            nn.Linear(12 * 12*8, 256), # Here, you need to match the flattened size
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(256, 128),
+            nn.Linear(12 * 12*8, 128), # Here, you need to match the flattened size
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(128, 64),
