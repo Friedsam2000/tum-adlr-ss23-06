f75ec3d622411c160b2d69f03146877b907feaef
samuel <samuelzeitler2000@gmail.com>
2023-08-04 15:11:56 +0200

    one more conv layer

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -11,7 +11,11 @@ class CNNExtractor(nn.Module):
             nn.ReLU(),
 
             # size is 24x24x32
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+
+            # size is 24x24x64
+            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
 
             # size is 12x12x64
