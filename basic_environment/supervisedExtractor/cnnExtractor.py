0e59d7cc9619564f08526e23361fc660ec70b772
samuel <samuelzeitler2000@gmail.com>
2023-08-04 14:57:16 +0200

    fully conv

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -7,43 +7,44 @@ class CNNExtractor(nn.Module):
 
         self.features = nn.Sequential(
             # size is 96x96x3
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-
-            # size is 47x47x32
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-
-            # size is 23x23x64
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-
-            # size is 11x11x128 formula: (W−K+2P)/S+1
-            nn.Conv2d(128, 128, kernel_size=2, stride=1),
+            nn.Conv2d(3, 32, kernel_size=4, stride=4, padding=1),
             nn.ReLU(),
 
-            # size is 10x10x128
-            nn.Conv2d(128, 64, kernel_size=2, stride=1),
+            # size is 24x24x32 (formula: (W−K+2P)/S+1)
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
 
-            # size is 9x9x64
-            nn.Conv2d(64, 16, kernel_size=2, stride=1),
+            # size is 12x12x64
+            nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=1),
             nn.ReLU(),
 
-            # size is 8x8x32
-            nn.Conv2d(16, 1, kernel_size=2, stride=1),
+            # size is 12x12x128
+            nn.Conv2d(128, 32, kernel_size=2, stride=1, padding=1),
             nn.ReLU(),
 
-            # size is 7x7x1
+            # size is 12x12x64
+            nn.Conv2d(32, 8, kernel_size=2, stride=1, padding=1),
+            nn.ReLU(),
 
 
         )
 
+        self.classifier = nn.Sequential(
+            # 4 fully connected layers
+            nn.Linear(12 * 12*8, 256), # Here, you need to match the flattened size
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(64, 49),
+        )
 
     def forward(self, x):
         x = self.features(x)
         x = torch.flatten(x, 1)
+        x = self.classifier(x)
         return x
