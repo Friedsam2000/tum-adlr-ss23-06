a9910ce62dda2fd8db8c134b483ca9381e382c3b
samuel <samuelzeitler2000@gmail.com>
2023-08-04 14:39:39 +0200

    more cnn 2

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -9,40 +9,49 @@ class CNNExtractor(nn.Module):
             # size is 96x96x3
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
 
             # size is 47x47x32
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
 
             # size is 23x23x64
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
 
-            # size is 23x23x128
+            # size is 11x11x128
             nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
 
-            # size is 23x23x64
+            # size is 11x11x64
             nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
 
-            # size is 23x23x32
+            # size is 11x11x32
+            nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+
+            # size is 11x11x16
+            nn.Conv2d(16, 8, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+
+
         )
 
         self.classifier = nn.Sequential(
             # 4 fully connected layers
-            nn.Linear(32 * 23*23, 512), # Here, you need to match the flattened size
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(512, 256),
+            nn.Linear(11 * 11*8, 256), # Here, you need to match the flattened size
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(256, 128),
             nn.ReLU(),
             nn.Dropout(0.1),
-            nn.Linear(128, 49),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(64, 49),
         )
 
     def forward(self, x):
