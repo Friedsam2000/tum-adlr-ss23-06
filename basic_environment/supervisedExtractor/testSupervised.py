e4d47901235f0d7662ca6881df98cf9aabecfc6c
samuel <samuelzeitler2000@gmail.com>
2023-08-05 13:13:37 +0200

    print

--- basic_environment/supervisedExtractor/testSupervised.py
+++ basic_environment/supervisedExtractor/testSupervised.py
@@ -5,6 +5,7 @@ from cnnExtractor import CNNExtractor
 from dataPreprocessor import load_data
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 # Define transformation for the images, consistent with training script
 transform = transforms.Compose([
@@ -20,9 +21,6 @@ csv_file_path = os.path.join(script_dir, '../img_data_generation/labels.csv')
 images_dir_path = os.path.join(script_dir, '../img_data_generation')
 dataset = load_data(csv_file=csv_file_path, images_dir=images_dir_path, transform=transform)
 
-# Get a random index from the dataset
-random_index = random.randint(0, len(dataset) - 1)
-
 # Instantiate the model
 model = CNNExtractor()
 
@@ -34,64 +32,77 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 model.eval()
 
-# Get the random sample from the dataset
-sample = dataset[random_index]
-image_tensor = sample['image'].to(device)
-
-# Pass the image through the model
-with torch.no_grad():
-    prediction = model(image_tensor.unsqueeze(0)).squeeze()
-
-# Extract and print the agent's position, goal position, and neighboring grid
-predicted_neighboring_grid_logits = prediction.reshape(7, 7)
-predicted_neighboring_grid = torch.sigmoid(predicted_neighboring_grid_logits).tolist()
-
-# Extract the true agent's position, goal position, and neighboring grid
-true_neighboring_grid = torch.Tensor(sample['label'][4:]).reshape(7, 7).tolist()
-
-# Show the image
-image_numpy = image_tensor.cpu().permute(1, 2, 0).numpy() # Convert to CPU for visualization
-plt.imshow(image_numpy)
-plt.axis('off') # To turn off axes
-plt.show()
-
-# Visualize the predicted and true grids
-threshold = 0.5
-print("Predicted NeighborGrid:")
-predicted_grid_visual = [['O' if cell >= threshold else 'X' for cell in row] for row in predicted_neighboring_grid]
-for row in predicted_grid_visual:
-    print(" ".join(row))
-print("True NeighborGrid:")
-true_grid_visual = [['O' if cell >= threshold else 'X' for cell in row] for row in true_neighboring_grid]
-for row in true_grid_visual:
-    print(" ".join(row))
-
-# Calculate false positives and false negatives
-false_positives = 0
-false_negatives = 0
-for i in range(7):
-    for j in range(7):
-        if predicted_neighboring_grid[i][j] >= threshold and true_neighboring_grid[i][j] < threshold:
-            false_positives += 1
-        if predicted_neighboring_grid[i][j] < threshold and true_neighboring_grid[i][j] >= threshold:
-            false_negatives += 1
-
-print("Number of false positives (falsely predicted obstacles):", false_positives)
-print("Number of false negatives (missed obstacles):", false_negatives)
-
-
 # Define the loss function (consistent with training)
 def custom_loss(predictions, grid_labels):
     predictions = predictions.view(predictions.size(0), -1)
-    pos_weight = torch.full_like(grid_labels, 1)
+    pos_weight = torch.full_like(grid_labels, 2)
     loss_grid = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)(predictions, grid_labels)
     return loss_grid
 
-# Convert true_neighboring_grid to a tensor
-true_neighboring_grid_tensor = torch.Tensor(true_neighboring_grid).to(device)
+# Variables to hold cumulative false positives, false negatives, and loss
+cumulative_false_positives = 0
+cumulative_false_negatives = 0
+cumulative_loss = 0
 
-# Calculate the loss
-loss = custom_loss(predicted_neighboring_grid_logits, true_neighboring_grid_tensor)
+# Number of predictions
+num_predictions = 100
 
-# Print the loss
-print("Loss of the prediction:", loss.item())
+# Threshold for grid classification
+threshold = 0.5
+
+for _ in range(num_predictions):
+    # Get a random index from the dataset
+    random_index = random.randint(0, len(dataset) - 1)
+
+    # Get the random sample from the dataset
+    sample = dataset[random_index]
+    image_tensor = sample['image'].to(device)
+
+    # Pass the image through the model
+    with torch.no_grad():
+        prediction = model(image_tensor.unsqueeze(0)).squeeze()
+
+    # Extract and print the agent's position, goal position, and neighboring grid
+    predicted_neighboring_grid_logits = prediction.reshape(7, 7)
+    predicted_neighboring_grid = torch.sigmoid(predicted_neighboring_grid_logits).tolist()
+
+    # Extract the true agent's position, goal position, and neighboring grid
+    true_neighboring_grid = torch.Tensor(sample['label'][4:]).reshape(7, 7).tolist()
+
+    # Calculate false positives and false negatives
+    false_positives = 0
+    false_negatives = 0
+    for i in range(7):
+        for j in range(7):
+            if predicted_neighboring_grid[i][j] >= threshold and true_neighboring_grid[i][j] < threshold:
+                false_positives += 1
+            if predicted_neighboring_grid[i][j] < threshold and true_neighboring_grid[i][j] >= threshold:
+                false_negatives += 1
+
+    # Add to cumulative counts
+    cumulative_false_positives += false_positives
+    cumulative_false_negatives += false_negatives
+
+    # Convert true_neighboring_grid to a tensor
+    true_neighboring_grid_tensor = torch.Tensor(true_neighboring_grid).to(device)
+
+    # Calculate the loss
+    loss = custom_loss(predicted_neighboring_grid_logits, true_neighboring_grid_tensor)
+
+    # Add to cumulative loss
+    cumulative_loss += loss.item()
+
+# Print the predicted grid of the last iteration
+print("Predicted NeighborGrid of last sample:")
+predicted_grid_visual = [['O' if cell >= threshold else 'X' for cell in row] for row in predicted_neighboring_grid]
+for row in predicted_grid_visual:
+    print(" ".join(row))
+
+# Calculate mean values
+mean_false_positives = cumulative_false_positives / num_predictions
+mean_false_negatives = cumulative_false_negatives / num_predictions
+mean_loss = cumulative_loss / num_predictions
+
+print("Mean number of false positives (falsely predicted obstacles):", mean_false_positives)
+print("Mean number of false negatives (missed obstacles):", mean_false_negatives)
+print("Mean loss of the predictions:", mean_loss)
