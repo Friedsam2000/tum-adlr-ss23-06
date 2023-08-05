f3d50db26b8617c6c9e656d8aa7073888eb083c4
samuel <samuelzeitler2000@gmail.com>
2023-08-05 16:58:52 +0200

    test update

--- basic_environment/supervisedExtractor/trainSupervised.py
+++ basic_environment/supervisedExtractor/trainSupervised.py
@@ -75,7 +75,7 @@ tb_log_dir = f'{log_dir}/{current_time}'
 writer = SummaryWriter(tb_log_dir)
 
 # Training loop
-num_epochs = 100
+num_epochs = 32
 for epoch in range(num_epochs):
     model.train()
     train_loss = 0.0
