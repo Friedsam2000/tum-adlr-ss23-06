a9ef4b42899845928492080fb63a116b7f3a785c
samuel <samuelzeitler2000@gmail.com>
2023-08-05 19:19:36 +0200

    train script fix?

--- basic_environment/supervisedExtractor/testSupervised.py
+++ basic_environment/supervisedExtractor/testSupervised.py
@@ -1,19 +1,38 @@
 import os
 import torch
+from torch.utils.data import DataLoader
 from torchvision import transforms
 from cnnExtractor import CNNExtractor
 from dataPreprocessor import load_data
-import matplotlib.pyplot as plt
-import random
-import numpy as np
+from torch.utils.data import Subset
+from datetime import datetime
+import torch.nn as nn
 
-# Define transformation for the images, consistent with training script
+
+# Check for CUDA availability and set the device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f'Using {device} device')
+
+# Define a custom loss function for grid prediction
+def custom_loss(predictions_grid, predictions_pos, grid_labels, pos_labels):
+    predictions_grid = predictions_grid.view(predictions_grid.size(0), -1)  # Reshaping predictions to match the target size
+    pos_weight = torch.full_like(grid_labels, 1)
+    loss_grid = nn.BCEWithLogitsLoss(pos_weight=pos_weight)(predictions_grid, grid_labels)
+
+    # MSE Loss for positions
+    loss_pos = nn.MSELoss()(predictions_pos, pos_labels)
+
+    # You can adjust the ratio of grid to position loss by using a different weight
+    grid_loss_weight = 1.0
+    pos_loss_weight = 0.001
+
+    return grid_loss_weight * loss_grid + pos_loss_weight * loss_pos
+
+
+# Define transformation for the images
 transform = transforms.Compose([
-    transforms.ToTensor()
+    transforms.ToTensor(),
 ])
-
-# Close all existing figure windows
-plt.close('all')
 
 # Load the dataset
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -21,100 +40,26 @@ csv_file_path = os.path.join(script_dir, '../img_data_generation/labels.csv')
 images_dir_path = os.path.join(script_dir, '../img_data_generation')
 dataset = load_data(csv_file=csv_file_path, images_dir=images_dir_path, transform=transform)
 
-# Instantiate the model
+# Create a data loader with multiple workers
+data_loader = DataLoader(dataset, batch_size=32, shuffle=False, num_workers=4, pin_memory=True)
+
+
+# Load the model
 model = CNNExtractor()
-
-# Load the model weights
-model.load_state_dict(torch.load('model.pth'))
-
-# Set the device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_path = 'model.pth'
+model.load_state_dict(torch.load(model_path))
 model.to(device)
-
-# Set model to evaluation mode
 model.eval()
 
-# Define the loss function (consistent with training)
-def custom_loss(predictions_grid, predictions_pos, grid_labels, pos_labels):
-    predictions_grid = predictions_grid.view(predictions_grid.size(0), -1)
-    pos_weight = torch.full_like(grid_labels, 1)
-    loss_grid = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)(predictions_grid, grid_labels)
-    loss_pos = torch.nn.MSELoss()(predictions_pos, pos_labels)
-    return loss_grid, loss_pos
+# Test the model
+test_loss = 0.0
+with torch.no_grad():
+    for batch in data_loader:
+        images = batch['image'].to(device)
+        grid_labels = batch['label'][:, 4:].clone().detach().float().to(device)  # Only obstacle grid, ignoring first 4 elements
+        pos_labels = batch['label'][:, :4].clone().detach().float().to(device)  # Only the positions
+        predictions_grid, predictions_pos = model(images)
+        combined_loss = custom_loss(predictions_grid, predictions_pos, grid_labels, pos_labels)
+        test_loss += combined_loss.item()
 
-# Variables to hold cumulative false positives, false negatives, and loss
-cumulative_false_positives = 0
-cumulative_false_negatives = 0
-cumulative_loss = 0
-
-# Number of predictions
-num_predictions = 100
-
-# Threshold for grid classification
-threshold = 0.5
-
-for _ in range(num_predictions):
-    # Get a random index from the dataset
-    random_index = random.randint(0, len(dataset) - 1)
-
-    # Get the random sample from the dataset
-    sample = dataset[random_index]
-    image_tensor = sample['image'].to(device)
-
-    # Pass the image through the model
-    with torch.no_grad():
-        prediction_grid, prediction_pos = model(image_tensor.unsqueeze(0))
-
-    # Squeeze prediction_pos to remove the extra dimension
-    prediction_pos = prediction_pos.squeeze()
-
-
-    # Extract and print the agent's position, goal position, and neighboring grid
-    predicted_neighboring_grid_logits = prediction_grid.squeeze().reshape(7, 7)
-    predicted_neighboring_grid = torch.sigmoid(predicted_neighboring_grid_logits).tolist()
-    predicted_positions = prediction_pos.squeeze().tolist()
-
-    # Extract the true agent's position, goal position, and neighboring grid
-    true_positions = sample['label'][:4]
-    true_neighboring_grid = torch.Tensor(sample['label'][4:]).reshape(7, 7).tolist()
-
-    # Calculate false positives and false negatives
-    false_positives = 0
-    false_negatives = 0
-    for i in range(7):
-        for j in range(7):
-            if predicted_neighboring_grid[i][j] >= threshold and true_neighboring_grid[i][j] < threshold:
-                false_positives += 1
-            if predicted_neighboring_grid[i][j] < threshold and true_neighboring_grid[i][j] >= threshold:
-                false_negatives += 1
-
-    # Add to cumulative counts
-    cumulative_false_positives += false_positives
-    cumulative_false_negatives += false_negatives
-
-    # Convert true_neighboring_grid to a tensor
-    true_neighboring_grid_tensor = torch.Tensor(true_neighboring_grid).to(device)
-    true_positions_tensor = torch.Tensor(true_positions).to(device)
-
-    # Calculate the loss
-    loss_grid, loss_pos = custom_loss(predicted_neighboring_grid_logits, prediction_pos, true_neighboring_grid_tensor, true_positions_tensor)
-
-    # Add to cumulative loss
-    cumulative_loss += loss_grid.item() + loss_pos.item()
-
-# Print the predicted grid and positions of the last iteration
-print("Predicted NeighborGrid of last sample:")
-predicted_grid_visual = [['O' if cell >= threshold else 'X' for cell in row] for row in predicted_neighboring_grid]
-for row in predicted_grid_visual:
-    print(" ".join(row))
-print("Predicted positions:", np.round(predicted_positions))
-print("True positions:", true_positions)
-
-# Calculate mean values
-mean_false_positives = cumulative_false_positives / num_predictions
-mean_false_negatives = cumulative_false_negatives / num_predictions
-mean_loss = cumulative_loss / num_predictions
-
-print("Mean number of false positives (falsely predicted obstacles):", mean_false_positives)
-print("Mean number of false negatives (missed obstacles):", mean_false_negatives)
-print("Mean loss of the predictions:", mean_loss)
+print(f"Test loss: {test_loss / len(data_loader)}")
