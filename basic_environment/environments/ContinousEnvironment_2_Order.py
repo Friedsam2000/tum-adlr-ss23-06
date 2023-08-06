a67704f4ead131bfe242f6baf8649a64618d4217
Matthias <matthias.kreiner29@gmail.com>
2023-08-06 23:55:11 +0200

    Test Stuff

--- basic_environment/environments/ContinousEnvironment_2_Order.py
+++ basic_environment/environments/ContinousEnvironment_2_Order.py
@@ -56,7 +56,7 @@ class CustomEnv_2order_dyn(gym.Env):
         # max velocity
         self.max_vel = 2.0
         #velocity delta at goal
-        self.goal_vel = 0.05
+        self.goal_vel = 10.0
 
         #Agent Mass
         self.agent_mass = 1.0
