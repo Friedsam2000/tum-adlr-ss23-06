b64105a94b6b691abf7bab83ceb09c524531be37
samuel <samuelzeitler2000@gmail.com>
2023-08-06 14:03:31 +0200

    only train on positions

--- basic_environment/reinforcementPretrainedExtractor/trainReinforcementPretrained.py
+++ basic_environment/reinforcementPretrainedExtractor/trainReinforcementPretrained.py
@@ -20,8 +20,11 @@ class CustomPolicy(ActorCriticPolicy):
         super(CustomPolicy, self).__init__(*args, **kwargs,
                                            features_extractor_class=PretrainedFeaturesExtractor,
                                            features_extractor_kwargs=dict(features_dim=4 * 4),
-                                           net_arch=[32, 64, 128, 64, 32],
-                                           )
+                                           net_arch=[32, 64,128,64,32])  # This defines the size of the MLP layers. Modify as needed.
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor = create_mlp(self.features_dim, self.net_arch, activation_fn=nn.ReLU)  # Add activation function
+
 
     def _build_mlp_extractor(self) -> None:
         self.mlp_extractor = create_mlp(self.features_dim, self.net_arch)
