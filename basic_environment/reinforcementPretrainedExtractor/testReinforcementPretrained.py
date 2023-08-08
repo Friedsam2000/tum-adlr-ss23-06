c3578e4e1aad1ef70010bdaa6d10964ce4056f15
samuel <samuelzeitler2000@gmail.com>
2023-08-08 16:31:55 +0200

    Revert "normalized agent pos"
    This reverts commit 42c2b8f25247d92c336456ea79aa999da2c10a8f.

--- basic_environment/reinforcementPretrainedExtractor/testReinforcementPretrained.py
+++ basic_environment/reinforcementPretrainedExtractor/testReinforcementPretrained.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 # Set up the Bucket (google cloud storage)
 # Define the bucket name
 bucket_name = 'adlr_bucket'
-model_directory = "basic_environment/reinforcementPretrained/models/DQN_7_0"
+model_directory = "basic_environment/reinforcementPretrained/models/DQN_1_0"
 
 # Define the local download path
 local_path = "models_from_bucket"
@@ -85,7 +85,7 @@ while episodes < num_episodes:
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, terminated, truncated, info = env.step(action)
 
-    env.render()
+    # env.render()
     if terminated:
         if reward == 1:
             goals_reached += 1
