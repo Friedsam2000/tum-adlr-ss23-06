3306178c39861c397abea2fa1e0bc2c3304a2f45
samuel <samuelzeitler2000@gmail.com>
2023-08-08 20:41:06 +0200

    cnnExtractor for 11x11 fully conv

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -9,7 +9,7 @@ class CNNExtractor(nn.Module):
         self.objectGridClassifier = nn.Sequential(
             # size 96x96x3
             nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
