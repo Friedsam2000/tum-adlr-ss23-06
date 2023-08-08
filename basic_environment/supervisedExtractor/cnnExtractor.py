d152c5b6a70f1ab0a42cc866e56e0ce165258ccb
samuel <samuelzeitler2000@gmail.com>
2023-08-09 01:24:37 +0200

    Revert "added dropout and batch norm back in for short time"
    This reverts commit b56b88ad7aaad96a0f47fec9dd74bcee597316bd.

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -9,46 +9,46 @@ class CNNExtractor(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
             # nn.Conv2d(observation_space.shape[0], 32, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(16),
+            # nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
+            # nn.Dropout(0.1),
 
             # size 48x48x16
 
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
+            # nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
+            # nn.Dropout(0.1),
 
             # size 24x24x32
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
+            # nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
+            # nn.Dropout(0.1),
 
             # size 12x12x64
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
+            # nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
+            # nn.Dropout(0.1),
 
             # size 6x6x256
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
+            # nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
+            # nn.Dropout(0.1),
 
             # size 3x3x512
             nn.Conv2d(256, 1024, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(1024),
+            # nn.BatchNorm2d(1024),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
+            # nn.Dropout(0.1),
 
             # size 1x1x1024
             nn.Flatten(),
