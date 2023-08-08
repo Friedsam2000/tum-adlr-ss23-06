0dfac7e7976d950511286dac5258876e595a1726
samuel <samuelzeitler2000@gmail.com>
2023-08-08 20:38:28 +0200

    cnnExtractor for 11x11 fully conv

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -4,17 +4,17 @@ class CNNExtractor(nn.Module):
     def __init__(self):
         super(CNNExtractor, self).__init__()
 
-        # Assume input has shape (img_size, img_size, 12)
+        # Assume input has shape (img_size, img_size, 3)
         # size 96x96x3
-        self.features = nn.Sequential(
+        self.objectGridClassifier = nn.Sequential(
+            # size 96x96x3
             nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
-            # nn.Conv2d(observation_space.shape[0], 32, kernel_size=2, stride=1, padding=1),
-            nn.BatchNorm2d(16),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
-            # size 48x48x16
 
+            # size 48x48x32
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
@@ -26,34 +26,35 @@ class CNNExtractor(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
 
             # size 12x12x64
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        )
+            # size 6x6x128
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        self.objectGridClassifier = nn.Sequential(
-            # 3 conv layer to 11x11x1
-            # size 12x12x64
-            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
+            # size 3x3x128
+            nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2, padding=2, output_padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            # size 7x7x64
+            nn.ConvTranspose2d(64, 32, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Dropout(0.1),
 
-            # size 12x12x32
-            nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-
-            # size 12x12x1
-            nn.Conv2d(16, 1, kernel_size=2, stride=1),
+            # size 11x11x32
+            nn.ConvTranspose2d(32, 1, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(1),
-            nn.ReLU(),
-
-            # size 11x11x1
-
+            nn.ReLU()
         )
+
 
         self.positionClassifier = nn.Sequential(
 
@@ -84,7 +85,6 @@ class CNNExtractor(nn.Module):
         )
 
     def forward(self, x):
-        x = self.features(x)
         x_grid = self.objectGridClassifier(x)
         x_position = self.positionClassifier(x)
         return x_grid, x_position
