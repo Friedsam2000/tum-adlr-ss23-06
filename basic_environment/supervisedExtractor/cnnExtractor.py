050fa3c740acb4a6d762384240e24e50ba85f485
samuel <samuelzeitler2000@gmail.com>
2023-08-08 21:07:31 +0200

    cnnExtractor for 11x11 fully conv

--- basic_environment/supervisedExtractor/cnnExtractor.py
+++ basic_environment/supervisedExtractor/cnnExtractor.py
@@ -4,63 +4,18 @@ class CNNExtractor(nn.Module):
     def __init__(self):
         super(CNNExtractor, self).__init__()
 
-        # Assume input has shape (img_size, img_size, 3)
+        # Assume input has shape (img_size, img_size, 12)
         # size 96x96x3
-        self.objectGridClassifier = nn.Sequential(
-            # size 96x96x3
-            nn.Conv2d(3, 8, kernel_size=4, stride=4),
-            nn.BatchNorm2d(8),
-            nn.ReLU(),
-
-            # size 24x24x32
-            nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # size 12x12x16
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # size 6x6x32
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # size 3x3x64
-            nn.Conv2d(64, 11*11, kernel_size=2,stride=1),
-            # size is 2x2x11*11
-            nn.BatchNorm2d(11*11),
-
-
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            # size is 1x1x11*11
-
-
-
-            nn.Flatten(),
-
-
-
-
-
-        )
-
-
-        self.positionClassifier = nn.Sequential(
-
-            # size 96x96x3
+        self.features = nn.Sequential(
             nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            # nn.Conv2d(observation_space.shape[0], 32, kernel_size=2, stride=1, padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.1),
 
-            # size 48x48x32
+            # size 48x48x16
+
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
@@ -72,31 +27,56 @@ class CNNExtractor(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
             # size 12x12x64
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
-            # size 6x6x128
+            # size 6x6x256
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
-            # size 3x3x256
+            # size 3x3x512
+            nn.Conv2d(256, 1024, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
 
-            # Flatten
+            # size 1x1x1024
             nn.Flatten(),
-            nn.Linear(3*3*256, 512),
+
+            # size 1024
+        )
+
+        self.objectGridClassifier = nn.Sequential(
+            nn.Linear(1024, 512),
             nn.ReLU(),
-            nn.Linear(512, 128),
+            nn.Linear(512, 256),
             nn.ReLU(),
-            nn.Linear(128, 4),
+            nn.Linear(256, 11*11),
+        )
+
+        self.positionClassifier = nn.Sequential(
+            nn.Linear(1024, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 4),
         )
 
     def forward(self, x):
+        x = self.features(x)
+        # print(x.shape)  # Add this line
         x_grid = self.objectGridClassifier(x)
         x_position = self.positionClassifier(x)
         return x_grid, x_position
