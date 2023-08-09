2170908de6a668be5ea272ed74c86572ee95e6f5
samuel <samuelzeitler2000@gmail.com>
2023-08-09 04:04:35 +0200

    added batchnorm and dropout to CustomCNN (reinforcementPretrained)

--- basic_environment/reinforcementPretrainedExtractor/CustomCNN.py
+++ basic_environment/reinforcementPretrainedExtractor/CustomCNN.py
@@ -2,36 +2,51 @@ from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 import torch.nn as nn
 import torch
 
+
 class CustomCNNExtractor(BaseFeaturesExtractor):
     def __init__(self, observation_space, features_dim=128):
         super(CustomCNNExtractor, self).__init__(observation_space, features_dim)
 
-
         self.cnn_grid = nn.Sequential(
-            nn.Conv2d(4, 32, kernel_size=3, stride=1, padding=1),   # [32, 11, 11]
+            nn.Conv2d(4, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),                           # [32, 5, 5]
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # [64, 5, 5]
+            nn.Dropout(0.1),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),                           # [64, 2, 2]
-            nn.Conv2d(64, 128, kernel_size=2, stride=1),            # [128, 1, 1]
-            nn.ReLU()
+            nn.Dropout(0.1),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(64, 128, kernel_size=2, stride=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Dropout(0.1)
         )
 
         self.cnn_pos = nn.Sequential(
-            nn.Conv2d(4, 32, kernel_size=3, stride=1, padding=1),   # [32, 11, 11]
+            nn.Conv2d(4, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),                           # [32, 5, 5]
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # [64, 5, 5]
+            nn.Dropout(0.1),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),                           # [64, 2, 2]
-            nn.Conv2d(64, 128, kernel_size=2, stride=1),            # [128, 1, 1]
-            nn.ReLU()
+            nn.Dropout(0.1),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(64, 128, kernel_size=2, stride=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Dropout(0.1)
         )
 
-        # Adjusted size calculation for 11x11 input. The final feature map size should be 1x1x128 for each cnn sequence.
         self.linear = nn.Sequential(
-            nn.Linear(128*1*1*2, features_dim),                     # [features_dim]
+            nn.Linear(128 * 1 * 1 * 2, features_dim),
             nn.ReLU(),
         )
 
@@ -39,11 +54,11 @@ class CustomCNNExtractor(BaseFeaturesExtractor):
         grid_data = observations[:, ::2, :, :]
         pos_data = observations[:, 1::2, :, :]
 
-        grid_features = self.cnn_grid(grid_data)                    # [batch_size, 128, 1, 1]
-        pos_features = self.cnn_pos(pos_data)                       # [batch_size, 128, 1, 1]
+        grid_features = self.cnn_grid(grid_data)
+        pos_features = self.cnn_pos(pos_data)
 
-        grid_features = grid_features.view(grid_features.size(0), -1)  # [batch_size, 128]
-        pos_features = pos_features.view(pos_features.size(0), -1)     # [batch_size, 128]
-        combined_features = torch.cat([grid_features, pos_features], dim=1)  # [batch_size, 256]
+        grid_features = grid_features.view(grid_features.size(0), -1)
+        pos_features = pos_features.view(pos_features.size(0), -1)
+        combined_features = torch.cat([grid_features, pos_features], dim=1)
 
-        return self.linear(combined_features)                              # [batch_size, features_dim]
+        return self.linear(combined_features)
